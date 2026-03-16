70510796316db0ca8755a1981a83656b6f341449
viq hulow <viq.hlw@gmail.com>
2026-03-16 10:23:47 +0100

    feat: fix mapper

--- components/adapters/RMTMapper.cpp
+++ components/adapters/RMTMapper.cpp
@@ -7,15 +7,15 @@ RMTMapper::RMTMapper(const int& resolution) : _resolution(resolution) {}
 rmt_symbol_word_t RMTMapper::map(const uint32_t& bit) {
     return bit == 1 
     ? rmt_symbol_word_t{ 
-        .duration0 = static_cast<uint16_t>(nsToTick(WS2815Timing::T1L)), 
+        .duration0 = static_cast<uint16_t>(nsToTick(WS2815Timing::T1H)), 
         .level0 = 1, 
-        .duration1 = static_cast<uint16_t>(nsToTick(WS2815Timing::T1H)), 
+        .duration1 = static_cast<uint16_t>(nsToTick(WS2815Timing::T1L)), 
         .level1 = 0 
     } 
     : rmt_symbol_word_t{ 
-        .duration0 = static_cast<uint16_t>(nsToTick(WS2815Timing::T0L)), 
+        .duration0 = static_cast<uint16_t>(nsToTick(WS2815Timing::T0H)), 
         .level0 = 1, 
-        .duration1 = static_cast<uint16_t>(nsToTick(WS2815Timing::T0H)), 
+        .duration1 = static_cast<uint16_t>(nsToTick(WS2815Timing::T0L)), 
         .level1 = 0 
     };
 }
